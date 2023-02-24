63dbd588ef7a58361394c177b3def83d8fbaaafe
Eric Hendrix <ehendrix0091@yahoo.com>
2023-02-24 17:41:46 -0600

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Eric Hendrix on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
